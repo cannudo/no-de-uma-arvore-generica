8bd0b785e4c9c9a8e0511e876cf7ea7b2141b6ee
Luan da Costa Redmann <luandacostaredmann@gmail.com>
2025-10-29 11:19:18 -0300

    feat: o toString() da gata

--- No.java
+++ No.java
@@ -3,4 +3,14 @@ public class No {
     private No noPai;
     private No noFilhoEsquerdo;
     private No noFilhoDireito;
+
+    public String toString() {
+        String dado = this.dado;
+        int largura = dado.length() + 4;
+        String retorno = "+" + "-".repeat(largura - 2) + "+\n";
+        retorno += "| " + dado + " |\n";
+        retorno += "+" + "-".repeat(largura - 2) + "+\n";
+
+        return retorno;
+    }
 }

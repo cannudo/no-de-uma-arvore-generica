e27fa78c700761717da663135141368c51d8b86c
Luan da Costa Redmann <luandacostaredmann@gmail.com>
2025-10-29 11:42:00 -0300

    feat: construtores, getters e setters

--- No.java
+++ No.java
@@ -4,10 +4,58 @@ public class No {
     private No noFilhoEsquerdo;
     private No noFilhoDireito;
 
+    public No() {
+        this.dado = null;
+        this.noPai = null;
+        this.noFilhoEsquerdo = null;
+        this.noFilhoDireito = null;
+    }
+
+    public No(String dado) {
+        this.dado = dado;
+        this.noPai = null;
+        this.noFilhoEsquerdo = null;
+        this.noFilhoDireito = null;
+    }
+
+    public String getDado() {
+        return dado;
+    }
+
+    public void setDado(String dado) {
+        this.dado = dado;
+    }
+
+    public No getNoPai() {
+        return noPai;
+    }
+
+    public void setNoPai(No noPai) {
+        this.noPai = noPai;
+    }
+
+    public No getNoFilhoEsquerdo() {
+        return noFilhoEsquerdo;
+    }
+
+    public void setNoFilhoEsquerdo(No noFilhoEsquerdo) {
+        this.noFilhoEsquerdo = noFilhoEsquerdo;
+    }
+
+    public No getNoFilhoDireito() {
+        return noFilhoDireito;
+    }
+
+    public void setNoFilhoDireito(No noFilhoDireito) {
+        this.noFilhoDireito = noFilhoDireito;
+    }
+
     public String toString() {
-        String dado = this.dado;
+        String dado = this.getDado();
         int largura = dado.length() + 4;
-        String retorno = "+" + "-".repeat(largura - 2) + "+\n";
+        String retorno = null;
+
+        retorno = "+" + "-".repeat(largura - 2) + "+\n";
         retorno += "| " + dado + " |\n";
         retorno += "+" + "-".repeat(largura - 2) + "+\n";
 
